621362f1eaec4368e7e14b1a4e18ad969bf8f8ef
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2021-07-14 15:30:18 +0200

    idk think look for yourself

---
+++ dc_bot/bot.py
@@ -0,0 +1,13 @@
+
+import discord
+
+# boilerplate code from https://discordpy.readthedocs.io/en/latest/intro.html#basic-concepts
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')
--- flask_app/application.py
+++ flask_app/application.py
@@ -1,10 +1,15 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/server")
+def add_server():
+    server_id = request.args.get("server_id")
+    if not server_id: return None
 
 app.run()
