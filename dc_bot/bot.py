487ce8a460a1895fdc7d4010ce34e7022ed338f5
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2021-07-21 12:05:42 +0200

    added warn, kick and ban commands

--- dc_bot/bot.py
+++ dc_bot/bot.py
@@ -1,5 +1,4 @@
-
-import os, discord, datetime, re, requests, json
+import os, discord, datetime, requests, json, asyncio
 from discord.ext import commands
 
 API_URL = "http://127.0.0.1:5000"
@@ -10,7 +9,8 @@ intents.members = True
 
 client = commands.Bot(command_prefix="/", intents=intents)
 
-def time():
+
+def _time():
     # gets current time
     date = datetime.datetime.now()
     return f"{date.year}/{date.month}/{date.day}~{date.strftime('%X')}"
@@ -21,62 +21,79 @@ def time():
 
 @client.event
 async def on_ready():
-    print('@ {1}\n{0.user}:({0.user.id}) logged in\n'.format(client, time()))
+    print('@ {1}\n{0.user}:({0.user.id}) logged in\n'.format(client, _time()))
+
 
 @client.event
 async def on_message(message):
-    if message.author==client.user:
+    if message.author == client.user:
         return
 
     await client.process_commands(message)
 
-    print("@ {1}\nFrom {0.author}:({0.author.id})\nMessage: {0.content}\n".format(message, time()))
+    print("@ {1}\nFrom {0.author}:({0.author.id})\nMessage: {0.content}\n".format(message, _time()))
+
 
 @client.event
 async def on_member_join(member):
-    
-    print("@ {1}\nUser: {0}:({0.id})\nJoined: {0.guild}:({0.guild.id})\n".format(member, time()))
+    print("@ {1}\nUser: {0}:({0.id})\nJoined: {0.guild}:({0.guild.id})\n".format(member, _time()))
+
 
 @client.event
 async def on_member_remove(member):
-    print("@ {1}\nUser: {0}:({0.id})\nLeft: {0.guild}:({0.guild.id})\n".format(member, time()))
+    print("@ {1}\nUser: {0}:({0.id})\nLeft: {0.guild}:({0.guild.id})\n".format(member, _time()))
 
 
 @client.event
 async def on_guild_remove(guild):
-    requests.request("POST", f"{API_URL}/api/remove_server", data={"server-id": guild.id})
+    requests.request(
+        "POST", f"{API_URL}/api/remove_server",
+        data={"server-id": guild.id}
+    )
 
 ###
 # Bot commands
 ###
 
-# TODO: create roll muted on setup
 @client.command(name="setup")
 @commands.has_permissions(administrator=True)
 async def _setup(ctx):
-    
+
     await ctx.send("Setting up {0.name}:({0.id})".format(ctx.guild))
 
     # gets bool from flask api
-    api_req = requests.post(f"{API_URL}/api/add_server", data={"server-id": ctx.guild.id, "server-name": ctx.guild.name})
+    api_res = requests.request(
+        "POST", f"{API_URL}/api/add_server",
+        data={"server-id": ctx.guild.id, "server-name": ctx.guild.name}
+    )
 
     # loads api response as dict
-    api_req = json.loads(api_req.text)
+    api_res = json.loads(api_res.text)
 
-    if api_req["exists"]:
+    # checks if server already exists
+    if api_res["exists"]:
         await ctx.send("Server already setup")
         return
 
     for member in ctx.guild.members:
-        requests.post(f"{API_URL}/api/add_user", data={"user-id": member.id, "user-name": member.name})
-        requests.post(f"{API_URL}/api/add_user_server", data={"user-id": member.id, "server-id": ctx.guild.id})
+        requests.request(
+            "POST", f"{API_URL}/api/add_user",
+            data={"user-id": member.id, "user-name": member.name}
+        )
+        requests.request(
+            "POST", f"{API_URL}/api/add_user_server",
+            data={"user-id": member.id, "server-id": ctx.guild.id}
+        )
 
     await ctx.send("Server setup complete")
 
-@client.command(name="mute")
-async def _mute(ctx, user: discord.Member, *, reason=None):
 
-    requests.get(f"{API_URL}/api/user_violation?user-id={user.id}&server-id={ctx.guild.id}&violation-id=2&reason={reason}")
+@client.command(name="mute")
+async def _mute(ctx, user: discord.Member, minutes: int=5, *, reason="No reason provided"):
+    requests.request(
+        "POST", f"{API_URL}/api/user_violation",
+        data={"user-id": user.id, "server-id": ctx.guild.id, "violation-id": 2, "reason": reason}
+    )
 
     role = discord.utils.get(ctx.guild.roles, name="muted")
 
@@ -88,24 +105,75 @@ async def _mute(ctx, user: discord.Member, *, reason=None):
 
     await user.add_roles(role, reason=reason)
 
-@client.command(name="unmute")
-async def _unmute(ctx, user: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name="muted")
+    embed = discord.Embed(title="User::Mute", color=0xff0000)
+    embed.add_field(name="User:", value=f"{user.name}", inline=False)
+    embed.add_field(name="Minutes:", value=f"{minutes}", inline=False)
+    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
+    await ctx.send(embed=embed)
 
-    if not role:
-        return
-
+    seconds = minutes*60
+    await asyncio.sleep(seconds)
     await user.remove_roles(role)
+
+
+@client.command(name="kick")
+async def _kick(ctx, user, *, reason="No reason provided"):
+    await user.kick(reason=reason)
+
+    embed = discord.Embed(title="User::Kick", color=0xff0000)
+    embed.add_field(name="User:", value=f"{user.name}", inline=False)
+    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.command(name="ban")
+async def _ban(ctx, user: discord.Member, *, reason="No reason provided"):
+    await user.ban(reason=reason)
+
+    embed = discord.Embed(title="User::Ban", color=0xff0000)
+    embed.add_field(name="User:", value=f"{user.name}", inline=False)
+    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.command(name="warn")
+async def _warn(ctx, user: discord.Member, *, reason="No reason provided"):
+    api_res = requests.request(
+        "POST", f"{API_URL}/api/user_violation",
+        data={"user-id": user.id, "server-id": ctx.guild.id, "violation-id": 1, "reason": reason}
+    )
+
+    # loads api response as dict
+    api_res = json.loads(api_res.text)
+
+    if api_res['warns'] > 0:
+        if api_res["warns"] in (3, 6, 8):
+            await _mute(ctx, user, reason=f"{api_res['warns']} warns")
+            return
+        elif api_res["warns"] in (5, 7, 9):
+            await _kick(ctx, user, reason=f"{api_res['warns']} warns")
+            return
+        else:
+            await _ban(ctx, user, reason=f"10 warns")
+            return
+
+    embed = discord.Embed(title="User::Warn", color=0xff0000)
+    embed.add_field(name="User:", value=f"{user.name}", inline=False)
+    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
+    await ctx.send(embed=embed)
 
 
 @client.command(name="sheesh")
 async def _sheesh(ctx):
     await ctx.send("https://cdn.discordapp.com/attachments/680928395399266314/865187562918117396/sheeesh.mp4")
 
+
 @client.command("whoasked")
 async def _whoasked(ctx, user):
     await ctx.message.delete()
-    await ctx.send(f"{user} https://cdn.discordapp.com/attachments/680928395399266314/866657138280890438/video0_3.mp4")
+    await ctx.send(
+        f"{user.mention}\nhttps://cdn.discordapp.com/attachments/680928395399266314/866657138280890438/video0_3.mp4"
+    )
 
 
 @client.command(name="p")
@@ -114,13 +182,5 @@ async def _print(ctx, *, message):
     await ctx.send(message)
 
 
-@client.command(name="post-test")
-async def _post(ctx):
-    data = {
-        "user-id": 69,
-        "user-name": "ksdfj"
-    }
-    requests.post(f"{API_URL}/api/add_user", data=data)
-
-if __name__=='__main__':
+if __name__ == "__main__":
     client.run(os.getenv("BOT_TOKEN"))
--- flask_app/application.py
+++ flask_app/application.py
@@ -1,6 +1,5 @@
 
 import sqlite3
-
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -26,15 +25,15 @@ def add_server():
         cur.execute(f"INSERT INTO servers(server_id, server_name) VALUES({server_id}, '{server_name}')")
         con.commit()
         con.close()
-        return (jsonify({"exists": False}), 200)
-    return (jsonify({"exists": True}), 200)
+        return jsonify({"exists": False}), 200
+    return jsonify({"exists": True}), 200
 
 
 @app.route("/api/remove_server", methods=["POST"])
 def remove_server():
     server_id = request.form["server-id"]
     
-    if not server_id: return ("", 500)
+    if not server_id: return "", 500
     
     con = sqlite3.connect("./cs50-fp.db")
     cur = con.cursor()
@@ -46,39 +45,37 @@ def remove_server():
     con.commit()
     con.close()
     
-    return ("", 200)
+    return "", 200
 
 
 @app.route("/api/add_user", methods=["POST"])
 def add_user():
-    # user_id = request.args["user-id"]
-    # user_name = request.args["user-name"]
     user_id = request.form["user-id"]
     user_name = request.form["user-name"]
 
     # checks that data is not None
-    if not user_id: return ("", 500)
-    if not user_name: return ("", 500)
+    if not user_id: return "", 500
+    if not user_name: return "", 500
 
     con = sqlite3.connect("./cs50-fp.db")
     cur = con.cursor()
-    
+
     if not cur.execute(f"SELECT user_id FROM users WHERE user_id={user_id}").fetchone():
         cur.execute(f"INSERT INTO users(user_id, user_name) VALUES({user_id}, '{user_name}')")
         con.commit()
         con.close()
 
-    return ("", 200)
+    return "", 200
 
 
 @app.route("/api/add_user_server", methods=["POST"])
 def add_user_server():
     user_id = request.form["user-id"]
     server_id = request.form["server-id"]
-    
-    if not user_id: return ("", 500)
-    if not server_id: return ("", 500)
-    
+
+    if not user_id: return "", 500
+    if not server_id: return "", 500
+
     con = sqlite3.connect("./cs50-fp.db")
     cur = con.cursor()
 
@@ -86,28 +83,35 @@ def add_user_server():
     con.commit()
     con.close()
 
-    return ("", 200)
+    return "", 200
 
 
-@app.route("/api/user_violation")
+@app.route("/api/user_violation", methods=["POST"])
 def user_violation():
-    user_id = request.args["user-id"]
-    server_id = request.args["server-id"]
-    violation_id = request.args["violation-id"]
-    reason = request.args["reason"]
+    user_id = request.form["user-id"]
+    server_id = request.form["server-id"]
+    violation_id = request.form["violation-id"]
+    reason = request.form["reason"]
 
-    if not user_id: return ("", 500)
-    if not server_id: return ("", 500)
-    if not violation_id: return ("", 500)
-    if not reason: return ("", 500)
+    if not user_id: return "", 500
+    if not server_id: return "", 500
+    if not violation_id: return "", 500
+    if not reason: return "", 500
 
     con = sqlite3.connect("./cs50-fp.db")
     cur = con.cursor()
 
-    cur.execute(f"INSERT INTO user_violations(user_id, server_id, violation_id, reason) VALUES({user_id}, {server_id}, {violation_id}, '{reason}')")
+    cur.execute(
+        f"INSERT INTO user_violations(user_id, server_id, violation_id, reason) VALUES({user_id}, {server_id}, {violation_id}, '{reason}')"
+    )
+
+    warn_count = cur.execute(
+        f"SELECT COUNT(violation_id) FROM user_violations WHERE user_id={user_id} AND server_id={server_id} AND violation_id=1"
+    ).fetchone()
+
     con.commit()
     con.close()
 
-    return ("", 200)
+    return jsonify({"warns": warn_count[0]}), 200
 
 app.run()
